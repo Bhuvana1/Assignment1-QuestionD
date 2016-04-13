042f71ba9cff6e9a14e782c2566cb0b38d46bf34
Bhuvana1 <deepakbhuvana@gmail.com>
2016-04-12 18:11:38 -0700

    Added Events

--- TestMerge.java
+++ TestMerge.java
@@ -2,11 +2,16 @@ public class TestMerge {
 public void Thoughtfortheday(){
 System.out.println("Welcome");
 }
+public void upcomingEvents(){
+System.out.println("Party at Deepak House");
+}
 
 public static void main (String[] args)
 {
 TestMerge tm = new TestMerge();
 tm.Thoughtfortheday();
+tm.upcomingEvents();
 }
+
 
 }
